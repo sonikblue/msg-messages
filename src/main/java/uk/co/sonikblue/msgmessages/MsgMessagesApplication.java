7660476921c1734257d6b0595cf002712fb8031d
Gareth Allan <sonikblue@users.noreply.github.com>
2017-09-02 06:22:02 +0100

    Initial version of messages service.

---
+++ src/main/java/uk/co/sonikblue/msgmessages/MsgMessagesApplication.java
@@ -0,0 +1,12 @@
+package uk.co.sonikblue.msgmessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgMessagesApplication.class, args);
+	}
+}
